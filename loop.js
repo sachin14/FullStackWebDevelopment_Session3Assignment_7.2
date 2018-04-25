5d0c887aeee90e9246efaae4cf7a62a5f7af43ec
sachin jamthe <>
2018-04-24 21:23:59 -0500

    First Commit

---
+++ loop.js
@@ -0,0 +1,18 @@
+
+//Declare a variable to be used
+var i;
+
+//Coding For loop
+console.log("Using for loop");
+for(i=5; i<=50; i=i+5){
+    console.log(i);
+    
+}
+
+//Coding while loop
+console.log("Using while loop");
+i=5;
+while(i<=50){
+    console.log(i);
+    i = i + 5;
+}
